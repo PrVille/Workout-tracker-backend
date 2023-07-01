fc0633c7ffe19b44d5ee639d91d6e505e1f91b01
ville <ville.prami@aalto.fi>
2023-07-01 19:42:33 +0300

    remove vercel cron

--- api/cron.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-  runtime: "edge",
-}
-
-export default (request: Request) => {
-  console.log(`Hello, from ${request.url} I'm now an Edge Function!`)
-}
--- dist/api/cron.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    runtime: "edge",
-};
-exports.default = (request) => {
-    console.log(`Hello, from ${request.url} I'm now an Edge Function!`);
-};
